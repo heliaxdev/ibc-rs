2a421c237ff221206d45556de9c0c096e148faba
R2D2 <jacob.turner870@gmail.com>
2022-08-21 14:59:51 +0200

    [feat] Upgraded to current tendermint implementation of v0.37

--- modules/src/test_utils.rs
+++ modules/src/test_utils.rs
@@ -4,7 +4,6 @@ use crate::prelude::*;
 use tendermint::{block, consensus, duration::Duration, evidence, public_key::Algorithm};
 
 use crate::signer::Signer;
-use tendermint::consensus::params::{TimeoutParams, SynchronyParams};
 
 // Needed in mocks.
 pub fn default_consensus_params() -> consensus::Params {
@@ -12,6 +11,7 @@ pub fn default_consensus_params() -> consensus::Params {
         block: block::Size {
             max_bytes: 22020096,
             max_gas: -1,
+            time_iota_ms: 1000,
         },
         evidence: evidence::Params {
             max_age_num_blocks: 100000,
@@ -22,19 +22,6 @@ pub fn default_consensus_params() -> consensus::Params {
             pub_key_types: vec![Algorithm::Ed25519],
         },
         version: Some(consensus::params::VersionParams::default()),
-        synchrony: SynchronyParams {
-            message_delay: Duration::from_millis(505),
-            precision: Duration::from_secs(12),
-        },
-        timeout: TimeoutParams {
-            propose: Duration::from_millis(3000),
-            propose_delta: Duration::from_millis(500),
-            vote: Duration::from_millis(1000),
-            vote_delta: Duration::from_millis(500),
-            commit: Duration::from_millis(1000),
-            bypass_commit_timeout: false,
-        },
-        abci: Default::default(),
     }
 }
 
